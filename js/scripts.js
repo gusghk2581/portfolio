fc7b810b36f9d45435b35cb397c60a04a4b19024
gusghk2581 <128011459+gusghk2581@users.noreply.github.com>
2023-08-04 16:51:13 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,45 @@
-/*!
-* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
+window.addEventListener('load',function(){
+    // 애니메이션스크롤 이벤트
+    AOS.init();
+    // 헤더스크롤이벤트
+    $(window).scroll(function(){
+        // console.log('스크롤중');
+        if($(this).scrollTop()>80){
+            $('#header .container').addClass('active');
+        }else{
+            $('#header .container').removeClass('active');
         }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+    })
+    // 인트로 글자 타이핑이벤트
+    let allTxt = "NICE TO MEET YOU!\nI'm Webpublisher";
+    let idx=0;
+    let typingData=''; 
+    let typTxt = document.querySelector('.typing-txt');
+    setInterval(function(){
+        idx=0;                
+        typingData='';
+        function typingFunction(){
+            setTimeout(function(){
+                if(idx<allTxt.length){
+                    typingData = typingData + allTxt[idx++];
+                    typTxt.innerText = typingData;
+                    typingFunction();
+                }
+            },100)
+        };
+        typingFunction();
+    },allTxt.length*120)
+    // 팝업창오픈
+    // let _openPop = $('#design .g-container li a');
+    // let _popBox = $('.pop-box');
+    // $(_openPop).click(function(){
+    //     $('.pop-box').addClass('active');
+    //     // console.log(popImg);
+    //     return false;
+    // })
+    // 팝업창닫기
+    // $('.pop-box .fas').click(function(){
+    //     $('.pop-box').removeClass('active');
+    // })
+})
 
-    };
-
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-});
